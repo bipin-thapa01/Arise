fa783b73763918853249e2d7ac4711d51448fa54
Bipin Thapa <yukichin638@gmail.com>
2026-03-31 13:48:22 +0545

    working on calendar feature

--- lib/Screens/Progress/progress.dart
+++ lib/Screens/Progress/progress.dart
@@ -1,3 +1,4 @@
+import 'package:fitness/Screens/Progress/calendar_page.dart';
 import 'package:flutter/material.dart';
 
 class Progress extends StatelessWidget {
@@ -5,6 +6,21 @@ class Progress extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back_ios_new),
+        ),
+        title: Text(
+          "Habit Calendar",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        titleSpacing: 0,
+      ),
+      body: CalendarPage(),
+    );
   }
 }
